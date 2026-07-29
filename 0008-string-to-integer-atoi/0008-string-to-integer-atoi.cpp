56bf1c060da4e1c780e886c874456c1f063d9855
KumkumRathore28 <23bcs12633@cuchd.in>
2026-07-29 20:21:31 +0530

    Time: 0 ms (100%), Space: 9.3 MB (21.2%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
     int myAtoi(string s) {
-        int i = 0;
-        int n = s.size();
 
+        int i = 0;
+        int n = s.length();
+        
         while (i < n && s[i] == ' ')
             i++;
 
@@ -14,22 +15,20 @@ public:
             i++;
         }
 
-        long long num = 0;
+        long long ans = 0;
 
         while (i < n && isdigit(s[i])) {
+
             int digit = s[i] - '0';
 
-            num = num * 10 + digit;
+            if (ans > (INT_MAX - digit) / 10) {
+                return sign == 1 ? INT_MAX : INT_MIN;
+            }
 
-            if (sign * num >= INT_MAX)
-                return INT_MAX;
-
-            if (sign * num <= INT_MIN)
-                return INT_MIN;
-
+            ans = ans * 10 + digit;
             i++;
         }
 
-        return sign * num;
+        return sign * ans;
     }
 };
