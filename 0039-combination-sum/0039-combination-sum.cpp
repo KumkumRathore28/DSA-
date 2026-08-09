518d5b093b722973caa4fb2fc40ee6bbda176d0e
KumkumRathore28 <23bcs12633@cuchd.in>
2026-08-09 22:58:42 +0530

    Time: 0 ms (100%), Space: 14.2 MB (59.81%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,27 +1,34 @@
 class Solution {
 public:
     vector<vector<int>> ans;
-    vector<int> path;
+    vector<int> temp;
 
-    void dfs(vector<int>& candidates, int target, int start) {
+    void backtrack(vector<int>& candidates, int target, int start) {
+
         if (target == 0) {
-            ans.push_back(path);
+            ans.push_back(temp);
             return;
         }
 
         for (int i = start; i < candidates.size(); i++) {
-            if (candidates[i] > target)
-                continue;
 
-            path.push_back(candidates[i]);
-            dfs(candidates, target - candidates[i], i); 
-            path.pop_back();
+            if (candidates[i] > target)
+                break;
+
+            temp.push_back(candidates[i]);
+
+            backtrack(candidates, target - candidates[i], i);
+
+            temp.pop_back();
         }
     }
 
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+
         sort(candidates.begin(), candidates.end());
-        dfs(candidates, target, 0);
+
+        backtrack(candidates, target, 0);
+
         return ans;
     }
 };
