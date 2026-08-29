30646c840eaa45ec54814b3368db191ee9a65c4a
KumkumRathore28 <23bcs12633@cuchd.in>
2026-08-29 17:07:45 +0530

    Time: 42 ms (85.83%), Space: 29.2 MB (28.44%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,31 +1,50 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
+
         vector<vector<int>> ans;
+
         sort(nums.begin(), nums.end());
-        int n = nums.size();
 
-        for (int i = 0; i < n - 2; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
+        for (int i = 0; i < nums.size() - 2; i++) {
 
-            int l = i + 1, r = n - 1;
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
 
-            while (l < r) {
-                int sum = nums[i] + nums[l] + nums[r];
+            int left = i + 1;
+            int right = nums.size() - 1;
+
+            while (left < right) {
+
+                int sum = nums[i] + nums[left] + nums[right];
 
                 if (sum == 0) {
-                    ans.push_back({nums[i], nums[l], nums[r]});
 
-                    while (l < r && nums[l] == nums[l + 1]) l++;
-                    while (l < r && nums[r] == nums[r - 1]) r--;
+                    ans.push_back({
+                        nums[i],
+                        nums[left],
+                        nums[right]
+                    });
 
-                    l++;
-                    r--;
+                    while (left < right &&
+                           nums[left] == nums[left + 1])
+                        left++;
+
+                    while (left < right &&
+                           nums[right] == nums[right - 1])
+                        right--;
+
+                    left++;
+                    right--;
                 }
-                else if (sum < 0)
-                    l++;
-                else
-                    r--;
+
+                else if (sum < 0) {
+                    left++;
+                }
+
+                else {
+                    right--;
+                }
             }
         }
 
